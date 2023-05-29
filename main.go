65b57a0e0673758c9e17a21414a2b6765bf0356c
weberc2 <weberc2@gmail.com>
2023-05-29 13:44:34 -0500

    experience groups and levels

--- main.go
+++ main.go
@@ -37,6 +37,34 @@ type Move struct {
 	FreezeRisk uint8
 }
 
+type Experience uint64
+
+type Level uint8
+
+type ExperienceGroup int
+
+const (
+	ExperienceGroupFast ExperienceGroup = iota
+	ExperienceGroupMediumFast
+	ExperienceGroupMediumSlow
+	ExperiemceGroupSlow
+)
+
+func (group ExperienceGroup) Level(exp Experience) Level {
+	switch group {
+	case ExperienceGroupFast:
+		return Level(4 * exp * exp * exp / 5)
+	case ExperienceGroupMediumFast:
+		return Level(exp * exp * exp)
+	case ExperienceGroupMediumSlow:
+		return Level(6 * exp * exp * exp / 5 - 15 * exp * exp + 100 * exp - 140)
+	case ExperiemceGroupSlow:
+		return Level(5 * exp * exp * exp / 4)
+	default:
+		panic(fmt.Sprintf("invalid ExperienceGroup: %d", group))
+	}
+}
+
 type SpeciesID int
 
 const (
@@ -52,4 +80,13 @@ type Species struct {
 	ID SpeciesID
 	Name string
 	Type [2]Type
+}
+
+type PokemonID string
+
+type Pokemon struct {
+	ID PokemonID
+	Name string
+	Species SpeciesID
+	Experience Experience
 }
